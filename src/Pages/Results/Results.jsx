0ef3902985d1bf8f06da58153c08783be6b5ebf4
Alula Azmach <alulaaz@gmail.com>
2024-12-04 14:28:27 +0300

    Category Routing

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LayOut from '../../Components/LayOut/LayOut'
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { productUrl } from '../../Api/endPoints';
+import classes from "../Results/Results.module.css";
+import ProductCard from "../../Components/Product/ProductCard";
+
+
 
 function Results() {
+
+
+  const fullUrl = new URL(
+    `/products/category/${categoryName}`,
+    productUrl
+  ).toString();
+  axios
+    .get(fullUrl)
+    .then((res) => {
+      setResults(res.data);
+    })
+    .catch((err) => {
+      console.log("Error fetching data:", err);
+    });
+
+
+  const [results, setResults] = useState([])
+  const {categoryName} = useParams()
+  useEffect(()=>{
+  axios.get(`${productUrl}/products/category/${categoryName}`)
+  .then((res)=>{
+    setResults(res.data)
+  }).catch((err)=>{
+    console.log(err)
+  })
+},[])
+
+
+
+
+
   return (
     <LayOut>
-      <div>Results</div>
+      <section>
+        <h1 style={{padding:"30px"}}>Results</h1>
+        <p style={{padding: "30px"}}>Category / {categoryName}</p>
+      <hr/>
+      <div className={classes.products_container}>
+        {results?.map((product) => (
+          <ProductCard key={product.id}
+          product={product} />
+        ))}
+
+      </div>
+
+
+      </section>
     </LayOut>
   );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,8 @@ import Signup from './Pages/Auth/Signup'
 import Payment from './Pages/Payment/Payment'
 import Orders from './Pages/Orders/Orders'
 import Cart from './Pages/Cart/Cart'
+import Results from './Pages/Results/Results'
+
 
 function Routing() {
   return (
@@ -14,6 +16,7 @@ function Routing() {
         <Route path="/auth" element={<Signup />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
