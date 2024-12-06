a994de4b93ebee0ad92ed28002473f5e7e9a05ec
Alula Azmach <alulaaz@gmail.com>
2024-12-06 16:11:09 +0300

    Header Sticky

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,69 +11,71 @@ const Header = () => {
   const [{basket},dispatch]=useContext(DataContext)
   return (
     <>
-      <section>
-        <div className={classes.header_container}>
-          {/* logo section */}
-          <div className={classes.logo_container}>
-            <Link to="/">
-              <img
-                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="amazon logo"
-              />
-            </Link>
-            {/* delivery */}
-            <div className={classes.delivery}>
-              <span>
-                {/* icon */}
-                <GrLocation />
-              </span>
-              <div>
-                <p>Deliverd to</p>
-                <span>Ethiopia</span>
+      <section className={classes.fixed}>
+        <section>
+          <div className={classes.header_container}>
+            {/* logo section */}
+            <div className={classes.logo_container}>
+              <Link to="/">
+                <img
+                  src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                  alt="amazon logo"
+                />
+              </Link>
+              {/* delivery */}
+              <div className={classes.delivery}>
+                <span>
+                  {/* icon */}
+                  <GrLocation />
+                </span>
+                <div>
+                  <p>Deliverd to</p>
+                  <span>Ethiopia</span>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* search */}
-          <div className={classes.search}>
-            <select name="" id="">
-              <option value="">All</option>
-            </select>
-            <input type="text" name="" id="" placeholder="search product" />
-            {/* icon */}
-            <FaSearch size={25} />
-          </div>
-          {/* right side link */}
-          <div className={classes.order_container}>
-            <Link to="" className={classes.language}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_the_United_States.png/1200px-Flag_of_the_United_States.png"
-                alt="US flag"
-              />
+            {/* search */}
+            <div className={classes.search}>
               <select name="" id="">
-                <option value="">EN</option>
+                <option value="">All</option>
               </select>
-            </Link>
-            {/* three components */}
-            <Link to="/Payment">
-              <p>Sign In</p>
-              <span>Account & Lists</span>
-            </Link>
-            {/* orders */}
-            <Link to="/Orders">
-              <p>returns</p>
-              <span>& orders</span>
-            </Link>
-            {/* cart */}
-            <Link to="/Cart" className={classes.cart}>
+              <input type="text" name="" id="" placeholder="search product" />
               {/* icon */}
-              <HiOutlineShoppingCart size={35} />
-              <span>{basket.length}</span>
-            </Link>
+              <FaSearch size={25} />
+            </div>
+            {/* right side link */}
+            <div className={classes.order_container}>
+              <Link to="" className={classes.language}>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_the_United_States.png/1200px-Flag_of_the_United_States.png"
+                  alt="US flag"
+                />
+                <select name="" id="">
+                  <option value="">EN</option>
+                </select>
+              </Link>
+              {/* three components */}
+              <Link to="/Payment">
+                <p>Sign In</p>
+                <span>Account & Lists</span>
+              </Link>
+              {/* orders */}
+              <Link to="/Orders">
+                <p>returns</p>
+                <span>& orders</span>
+              </Link>
+              {/* cart */}
+              <Link to="/Cart" className={classes.cart}>
+                {/* icon */}
+                <HiOutlineShoppingCart size={35} />
+                <span>{basket.length}</span>
+              </Link>
+            </div>
           </div>
-        </div>
+        </section>
+        <LowerHeader />
       </section>
-      <LowerHeader />
     </>
   );
 };
